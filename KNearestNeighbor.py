3c2914f84a1a4f471f4625a0a5560a24b55acc5c
w41q <alexwang041@gmail.com>
2015-07-21 13:36:43 -0700

    Initial

---
+++ KNearestNeighbor.py
@@ -0,0 +1,98 @@
+import numpy as np
+import csv
+reader = csv.reader(open('dt_data.data', 'r'))
+dicts = []
+limit = 20000
+counter = 0
+for row in reader:
+	counter = counter+1
+	dict = {}
+	dict['age'] = long(row[0])
+	dict['workclass'] = row[1]
+	dict['fnlwgt'] = long(row[2])
+	dict['education'] = row[3]
+	dict['education-num'] = long(row[4])
+	dict['marital-status'] = row[5]
+	dict['occupation'] = row[6]
+	dict['relationship'] = row[7]
+	dict['race'] = row[8]
+	dict['sex'] = row[9]
+	dict['capital-gain'] = long(row[10])
+	dict['capital-loss'] = long(row[11])
+	dict['hours-per-week'] = long(row[12])
+	dict['native-country'] = row[13]
+	dicts.append(dict)
+	if(counter==limit):
+		break
+
+from sklearn.feature_extraction import DictVectorizer
+vec = DictVectorizer()
+data = vec.fit_transform(dicts).toarray()
+# trainname = vec.get_feature_names()
+# f = open('trainname','w')
+# for i in range(0,len(trainname)):
+# 	f.write(trainname[i]+'\n')
+# f.close()
+print(len(data))
+print(len(data[0]))
+reader = csv.reader(open('dt_label.data', 'r'))
+label0 = []
+counter = 0
+for row in reader:
+	counter = counter+1
+	label0.append(row)
+	if(counter==limit):
+		break
+print(len(label0))
+label = np.array(label0)
+
+from sklearn.neighbors import KNeighborsClassifier
+clf = KNeighborsClassifier(n_neighbors=3, algorithm='auto')
+clf.fit(data, label)
+
+a = np.array(['L','H'])
+
+print(clf.predict(data[6])==a[0])
+print(clf.predict(data[7])==a[1])
+
+reader = csv.reader(open('dt_data.test', 'r'))
+dicts2 = []
+for row in reader:
+	dict = {}
+	dict['age'] = long(row[0])
+	dict['workclass'] = row[1]
+	dict['fnlwgt'] = long(row[2])
+	dict['education'] = row[3]
+	dict['education-num'] = long(row[4])
+	dict['marital-status'] = row[5]
+	dict['occupation'] = row[6]
+	dict['relationship'] = row[7]
+	dict['race'] = row[8]
+	dict['sex'] = row[9]
+	dict['capital-gain'] = long(row[10])
+	dict['capital-loss'] = long(row[11])
+	dict['hours-per-week'] = long(row[12])
+	dict['native-country'] = row[13]
+	dicts2.append(dict)
+
+testdata = vec.fit_transform(dicts2).toarray()
+print(len(testdata))
+print(len(testdata[0]))
+# testname = vec.get_feature_names()
+# f = open('testname','w')
+# for i in range(0,len(testname)):
+# 	f.write(testname[i]+'\n')
+# f.close()
+reader = csv.reader(open('dt_label.test', 'r'))
+testlabel0 = []
+for row in reader:
+	testlabel0.append(row)
+print(len(testlabel0))
+testlabel = np.array(testlabel0)
+
+count = 0
+for i in range(0,len(testdata)):
+	if(clf.predict(testdata[i])==testlabel[i]):
+		count = count+1
+print(count)
+print(1.0*count/len(testdata))
---
+++ preprocess.java
@@ -0,0 +1,354 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class preprocess
+{
+	public static void main(String[] args)
+	{
+		//eliminateMissing();
+		//range();
+		//discretize();
+		//splitdata();
+		splitdata_dt();
+	}
+	
+	public static void eliminateMissing()
+	{
+		try
+		{
+			BufferedReader br = new BufferedReader(new FileReader("C:/15Spring/INF550/Homeworks/Project/adult/adult.test"));
+			File file = new File("C:/15Spring/INF550/Homeworks/Project/adult/adult_no_missing.test");
+			if (!file.exists())
+			{
+				file.createNewFile();
+			}
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			int count = 0, count_missing = 0;
+			String sCurrentLine;
+			long startTime = System.nanoTime();
+			while ((sCurrentLine = br.readLine()) != null)
+			{
+				count++;
+				if(sCurrentLine.contains("?"))
+				{
+					count_missing++;
+					continue;
+				}
+				if(sCurrentLine == null || sCurrentLine.isEmpty())
+				{
+					System.out.println(count+"\nNULL!!!");
+					break;
+				}
+				String fields[] = sCurrentLine.split(",");
+				String line = "";
+				for(int i=0;i<fields.length;i++)
+				{
+					line += fields[i].trim()+",";
+				}
+				line = line.substring(0, line.length()-1);
+				bw.write(line+"\n");
+			}
+
+			if(bw!=null) bw.close();
+			if (br != null)br.close();
+			long endTime = System.nanoTime();
+			System.out.println((endTime-startTime)/1000000000+"s");
+			System.out.println("records: " + count);
+			System.out.println("missing: " + count_missing);
+			System.out.println("Done");
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static void range()
+	{
+		try
+		{
+			int fieldnum = 12;
+			BufferedReader br = new BufferedReader(new FileReader("C:/15Spring/INF550/Homeworks/Project/adult/adult_no_missing.test"));
+			File file = new File("C:/15Spring/INF550/Homeworks/Project/adult/"+fieldnum+".test");
+			if (!file.exists())
+			{
+				file.createNewFile();
+			}
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			int count = 0;
+			long max = Long.MIN_VALUE, min = Long.MAX_VALUE;
+			String sCurrentLine;
+			long startTime = System.nanoTime();
+			while ((sCurrentLine = br.readLine()) != null)
+			{
+				count++;
+				if(sCurrentLine == null || sCurrentLine.isEmpty())
+				{
+					System.out.println(count+"\nNULL!!!");
+					break;
+				}
+				String fields[] = sCurrentLine.split(",");
+				bw.write(fields[fieldnum]+"\n");
+				long value = Long.parseLong(fields[fieldnum]);
+				if(value>max)
+					max = value;
+				if(value<min)
+					min = value;
+			}
+
+			if(bw!=null) bw.close();
+			if (br != null)br.close();
+			long endTime = System.nanoTime();
+			System.out.println((endTime-startTime)/1000000000+"s");
+			System.out.println("records: " + count);
+			System.out.println("max: " + max);
+			System.out.println("min: " + min);
+			System.out.println("Done");
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static void discretize()
+	{
+		try
+		{
+			BufferedReader br = new BufferedReader(new FileReader("C:/15Spring/INF550/Homeworks/Project/adult/adult_no_missing.test"));
+			File file = new File("C:/15Spring/INF550/Homeworks/Project/adult/discretize.test");
+			if (!file.exists())
+			{
+				file.createNewFile();
+			}
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			int count = 0;
+			String sCurrentLine;
+			long startTime = System.nanoTime();
+			while ((sCurrentLine = br.readLine()) != null)
+			{
+				count++;
+				if(sCurrentLine == null || sCurrentLine.isEmpty())
+				{
+					System.out.println(count+"\nNULL!!!");
+					break;
+				}
+				String fields[] = sCurrentLine.split(",");
+				String value0 = "", value2 = "", value4 = "", value10 = "", value11 = "", value12 = "";
+				
+				value0 = "1" + (char)(Integer.parseInt(fields[0])/10+'a');
+				
+				long value = Long.parseLong(fields[2]);
+				if(value<100000)
+					value2 = "2a";
+				else if(value>=100000&&value<200000)
+					value2 = "2b";
+				else if(value>=200000&&value<300000)
+					value2 = "2c";
+				else if(value>=300000&&value<400000)
+					value2 = "2d";
+				else if(value>=400000)
+					value2 = "2e";
+				
+				value = Long.parseLong(fields[4]);
+				value4 = "4" + (char)(value+'a');
+				
+				value = Long.parseLong(fields[10]);
+				if(value==0)
+					value10 = "10a";
+				else if(value<10000)
+					value10 = "10b";
+				else if(value<20000)
+					value10 = "10c";
+				else
+					value10 = "10d";
+				
+				value = Long.parseLong(fields[11]);
+				if(value==0)
+					value11 = "11a";
+				else if(value<1800)
+					value11 = "11b";
+				else if(value<2000)
+					value11 = "11c";
+				else
+					value11 = "11d";
+				
+				value = Long.parseLong(fields[12]);
+				if(value<30)
+					value12 = "12a";
+				else if(value<40)
+					value12 = "12b";
+				else if(value<45)
+					value12 = "12c";
+				else if(value<50)
+					value12 = "12d";
+				else if(value<60)
+					value12 = "12e";
+				else
+					value12 = "12f";
+				
+				String result = "";
+				for(int i=0;i<fields.length;i++)
+				{
+					if(i==0)
+						result += value0;
+					else if(i==2)
+						result += value2;
+					else if(i==4)
+						result += value4;
+					else if(i==10)
+						result += value10;
+					else if(i==11)
+						result += value11;
+					else if(i==12)
+						result += value12;
+					else
+						result += fields[i];
+					result += ",";
+				}
+				
+				bw.write(result.substring(0, result.length()-1)+"\n");
+//				if(count==5)
+//					break;
+			}
+
+			if(bw!=null) bw.close();
+			if (br != null)br.close();
+			long endTime = System.nanoTime();
+			System.out.println((endTime-startTime)/1000000000+"s");
+			System.out.println("records: " + count);
+			System.out.println("Done");
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static void splitdata()
+	{
+		try
+		{
+			BufferedReader br = new BufferedReader(new FileReader("C:/15Spring/INF550/Homeworks/Project/adult/discretize.test"));
+			
+			File filedata = new File("C:/15Spring/INF550/Homeworks/Project/adult/discretize_data.test");
+			if (!filedata.exists())
+			{
+				filedata.createNewFile();
+			}
+			FileWriter fwdata = new FileWriter(filedata.getAbsoluteFile());
+			BufferedWriter bwdata = new BufferedWriter(fwdata);
+			
+			File filelabel = new File("C:/15Spring/INF550/Homeworks/Project/adult/label.test");
+			if (!filelabel.exists())
+			{
+				filelabel.createNewFile();
+			}
+			FileWriter fwlabel = new FileWriter(filelabel.getAbsoluteFile());
+			BufferedWriter bwlabel = new BufferedWriter(fwlabel);
+			
+			int count = 0;
+			String sCurrentLine;
+			long startTime = System.nanoTime();
+			while ((sCurrentLine = br.readLine()) != null)
+			{
+				count++;
+				if(sCurrentLine == null || sCurrentLine.isEmpty())
+				{
+					System.out.println(count+"\nNULL!!!");
+					break;
+				}
+				String fields[] = sCurrentLine.split(",");
+				String linedata = "";
+				for(int i=0;i<fields.length-1;i++)
+				{
+					linedata += fields[i]+",";
+				}
+				bwdata.write(linedata.substring(0, linedata.length()-1)+"\n");
+				if(fields[fields.length-1].equals(">50K."))
+					bwlabel.write("H\n");
+				else
+					bwlabel.write("L\n");
+			}
+
+			if(bwdata!=null) bwdata.close();
+			if(bwlabel!=null) bwlabel.close();
+			if (br != null)br.close();
+			long endTime = System.nanoTime();
+			System.out.println((endTime-startTime)/1000000000+"s");
+			System.out.println("records: " + count);
+			System.out.println("Done");
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static void splitdata_dt()
+	{
+		try
+		{
+			BufferedReader br = new BufferedReader(new FileReader("C:/15Spring/INF550/Homeworks/Project/adult/decisiontree/adult_no_missing.test"));
+			
+			File filedata = new File("C:/15Spring/INF550/Homeworks/Project/adult/decisiontree/dt_data.test");
+			if (!filedata.exists())
+			{
+				filedata.createNewFile();
+			}
+			FileWriter fwdata = new FileWriter(filedata.getAbsoluteFile());
+			BufferedWriter bwdata = new BufferedWriter(fwdata);
+			
+			File filelabel = new File("C:/15Spring/INF550/Homeworks/Project/adult/decisiontree/dt_label.test");
+			if (!filelabel.exists())
+			{
+				filelabel.createNewFile();
+			}
+			FileWriter fwlabel = new FileWriter(filelabel.getAbsoluteFile());
+			BufferedWriter bwlabel = new BufferedWriter(fwlabel);
+			
+			int count = 0;
+			String sCurrentLine;
+			long startTime = System.nanoTime();
+			while ((sCurrentLine = br.readLine()) != null)
+			{
+				count++;
+				if(sCurrentLine == null || sCurrentLine.isEmpty())
+				{
+					System.out.println(count+"\nNULL!!!");
+					break;
+				}
+				String fields[] = sCurrentLine.split(",");
+				String linedata = "";
+				for(int i=0;i<fields.length-1;i++)
+				{
+					linedata += fields[i]+",";
+				}
+				bwdata.write(linedata.substring(0, linedata.length()-1)+"\n");
+				if(fields[fields.length-1].equals(">50K."))
+					bwlabel.write("H\n");
+				else
+					bwlabel.write("L\n");
+			}
+
+			if(bwdata!=null) bwdata.close();
+			if(bwlabel!=null) bwlabel.close();
+			if (br != null)br.close();
+			long endTime = System.nanoTime();
+			System.out.println((endTime-startTime)/1000000000+"s");
+			System.out.println("records: " + count);
+			System.out.println("Done");
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
